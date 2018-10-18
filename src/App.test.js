b016bcf5c5badcf8a78e9da79ddf2fbdaf2c50d5
hansmu <hans.miikael@gmail.com>
2018-10-18 20:10:20 +0300

    Remove boilerplate and add first test using custom attribute - a custom attribute is less likely to be changed by another developer than an ID or class, as an attribute isn't necessarily used for application running, while ID and class can have meaning for running

--- src/App.test.js
+++ src/App.test.js
@@ -5,8 +5,25 @@ import App from './App';
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const wrapper = shallow(<App/>);
-  console.log(wrapper.debug()); //Prints out the entire DOM of the node, easier to see what's going on in your rendered wrapper
-  expect(wrapper).toBeTruthy();
+  // console.log(wrapper.debug()); //Prints out the entire DOM of the node, easier to see what's going on in your rendered wrapper
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('clicking button increments counter display', () => {
+
 });
