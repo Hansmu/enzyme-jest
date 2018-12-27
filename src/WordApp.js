65f99e17f2e5cd4b675b5884608e5528d954f6f1
hansmu <hans.miikael@gmail.com>
2018-12-27 18:23:28 +0200

    Add given props tests

--- src/WordApp.js
+++ src/WordApp.js
@@ -3,17 +3,26 @@ import {connect} from 'react-redux';
 
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import Input from './Input';
+
+import {getSecretWord} from "./actions";
 
 class WordApp extends Component {
     render(){
         return (
             <div className={'container'}>
                 <h1>Word Guessing Thing</h1>
-                <Congrats success={false}/>
-                <GuessedWords guessedWords={[]}/>
+                <Congrats success={this.props.success}/>
+                <Input />
+                <GuessedWords guessedWords={this.props.guessedWords}/>
             </div>
         );
     }
 }
 
-export default WordApp;
+const mapStateToProps = state => {
+    const {success, guessedWords, secretWord} = state;
+    return {success, guessedWords, secretWord};
+};
+
+export default connect(mapStateToProps, {getSecretWord})(WordApp);
